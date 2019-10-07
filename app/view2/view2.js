72a3ebbb620e28b5ee7960f7deaef4aa55284b98
Carlos Talavera <carlostalaver@gmail.com>
2019-10-07 12:30:48 -0300

    aplicando validaciones

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -9,6 +9,47 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', [function() {
+.controller('View2Ctrl', ["$scope", function($scope) {
+  var init = function(){
+    $scope.data = {
+      paymentType: '', 
+      carNumber: '', 
+      nro_autorizacion: '', 
+      nro_comprobante: '', 
+      email: '', 
+      send: false
+    };
+  };
 
+
+  // OPTION TIPO DE PAGO
+  $scope.opciones = {
+    "1": {
+      "id": "E", 
+      "text": "Efectivo"
+    }, 
+    "2": {
+      "id": "D", 
+      "text": "Tarjeta de Débito"
+    }, 
+    "3": {
+      "id": "C", 
+      "text": "Tarjeta de Crédito"
+    }
+  }; 
+
+  var registroPago = function(){
+    $scope.registroPagoOK= true; 
+  };
+
+  $scope.submitForm2 = function(oForm){
+    $scope.data.send = true; 
+    if(oForm.$valid){
+      registroPago();
+    } else {
+      alert('Error');
+    }
+  };
+
+  init();
 }]);
